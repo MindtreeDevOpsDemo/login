f1b035b3e2675078a129ef79ea5d2476715d71ac
MindtreeDevOpsDemo <mindtreedevopsdemo@gmail.com>
2015-08-26 19:37:53 +0530

    Jenkins Auto Compilation

--- src/main/java/com/mindtree/demo/db/DataBase.java
+++ src/main/java/com/mindtree/demo/db/DataBase.java
@@ -30,6 +30,7 @@ public class DataBase {
 			// Do something with the Connection
 		} catch (SQLException ex) {
 			// handle any errors
+                    
 			System.out.println("SQLException: " + ex.getMessage());
 			System.out.println("SQLState: " + ex.getSQLState());
 			System.out.println("VendorError: " + ex.getErrorCode());

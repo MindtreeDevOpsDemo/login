cec3b5ec5a9d8b16d29dd09ab2116858fc195115
MindtreeDevOpsDemo <mindtreedevopsdemo@gmail.com>
2015-08-27 19:26:18 +0530

    latest demo for Ram

--- src/main/java/com/mindtree/demo/ValidateLogin.java
+++ src/main/java/com/mindtree/demo/ValidateLogin.java
@@ -13,31 +13,17 @@ import javax.servlet.http.HttpServletResponse;
 import com.mindtree.demo.db.DataBase;
 
 
-/**
- * Servlet implementation class ValidateLogin
- */
+
 public class ValidateLogin extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+  
     public ValidateLogin() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-/*	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("WELCOME: TO DEMO");
-	}*/
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
     @Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
